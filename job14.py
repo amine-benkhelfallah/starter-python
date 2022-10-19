2a9a82ee98700f9469c55acf46facfdd7bb2e25d
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-10-19 22:41:03 +0200

    Programme avec la fonction myFunction()

---
+++ job14.py
@@ -0,0 +1,10 @@
+# Fonctions
+# Créer un programme job14.py. Le programme devra contenir une fonction myFunction()
+# qui écrit dans le terminal “je suis une fonction”.
+
+
+# A l’aide du mot-clé (def) une fonction est définie 
+def myFunction():
+    print("Je suis une fonction")
+
+myFunction()     #toujours appeler ma fonction pour executer 
