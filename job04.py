a0ebdbb3123afc463a40741be9ca20f5e9f2b718
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-10-19 12:06:34 +0200

    Avec boucle while, affichage de nbrs de 0 à l’input

---
+++ job04.py
@@ -0,0 +1,8 @@
+#Avec une boucle while & une fonction system, affichez nombres se trouvant de 0 (inclus) à l’input (inlus).
+n = input('entrez une valeur : ')
+n=int(n)
+i=0
+while (i<=n):
+    print(i)
+    i=i+1
+
