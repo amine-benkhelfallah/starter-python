3fa75510aea7d7a256282c845ad16222c6665327
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-10-19 23:12:48 +0200

    Programme avec fonction écrit tous les paramètres dans le terminal

--- job16.py
+++ job16.py
@@ -1,4 +1,5 @@
 #Fonctions et paramètres 
+
 # Créer un programme qui devra contenir une fonction qui prend en
 # paramètres un nombre de paramètres indéfini.
 # La fonction écrira tous les paramètres dans le terminal.
@@ -6,5 +7,6 @@
 
 def myFunction ( *params ):
     print(params)
-# Exemple :  liste d'argument : "html", "CSS", "javascript", "bébé", "école", "pomme"
-myFunction("html", "CSS", "javascript", "bébé", "école", "pomme")
+
+# Exemple :  liste d'argument : "html", "CSS", "bébé", "école", "pomme"
+myFunction("html", "CSS", "bébé", "école", "pomme")
