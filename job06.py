7ff0781e4557350f1de9c845506e9a7cddb5481c
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-10-19 12:30:24 +0200

    Les boucles infinies, Bonjour [200~ Au revoir AA@DESKTOP-U15881 MINGW64 ~ $ cd OneDrive
    AA@DESKTOP-U15881 MINGW64 ~/OneDrive
    $ cd Bureau
    
    AA@DESKTOP-U15881 MINGW64 ~/OneDrive/Bureau
    $ ls
    'Call of Duty Dawnville Demo.lnk'*
    'Cisco Packet Tracer.lnk'*
     Cisco_Packet_Tracer_820_Windows_64bit_1048513af2.exe*
     GIT/
    'Git Shell.lnk'*
     GitHub.appref-ms
    'Nouveau dossier (2)'/
     Obsidian.lnk*
     VMware-player-full-16.2.4-20089737.exe*
     Viber.lnk*
    'Visual Studio Code.lnk'*
    'Word 2016.lnk'*
     cisco/
     debian-11.5.0-amd64-DVD-1.iso
     desktop.ini
     github/
    'kick off'/
     python/
     rufus-3.20.exe*
     script/
     test/
     test_git_win_python/
    'uTorrent Web.lnk'*
     ubuntu-22.04.1-desktop-amd64.iso
     work/
    '~/usr/bin/bash.odt'
    '~.odt'
    '~ de windows.odt'
    '~WRL0002.tmp'
    '~WRL1689.tmp'
    
    AA@DESKTOP-U15881 MINGW64 ~/OneDrive/Bureau
    $ cdpython
    bash: cdpython: command not found
    
    AA@DESKTOP-U15881 MINGW64 ~/OneDrive/Bureau
    $ cd python
    
    AA@DESKTOP-U15881 MINGW64 ~/OneDrive/Bureau/python
    $ ls
     Job10a.py        job03.py   job08.py       job100.py   output.txt.txt
    'de 4 a 10.png'   job04.py   job08haik.py   job11.py    test7
     domains.xml      job05.py   job09.py       job111.py
     job01.py         job06.py   job09ya.py     job11a.py
     job02.py         job07.py   job10.py       job8amine
    
    AA@DESKTOP-U15881 MINGW64 ~/OneDrive/Bureau/python
    $ git status
    fatal: not a git repository (or any of the parent directories): .git
    
    AA@DESKTOP-U15881 MINGW64 ~/OneDrive/Bureau/python
    $ git clone https://github.com/amine-benkhelfallah/starter-python
    Cloning into 'starter-python'...
    remote: Enumerating objects: 3, done.
    remote: Counting objects: 100% (3/3), done.
    remote: Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
    Receiving objects: 100% (3/3), done.
    
    AA@DESKTOP-U15881 MINGW64 ~/OneDrive/Bureau/python
    $ ls
     Job10a.py        job03.py   job08.py       job100.py   output.txt.txt
    'de 4 a 10.png'   job04.py   job08haik.py   job11.py    starter-python/
     domains.xml      job05.py   job09.py       job111.py   test7
     job01.py         job06.py   job09ya.py     job11a.py
     job02.py         job07.py   job10.py       job8amine
    
    AA@DESKTOP-U15881 MINGW64 ~/OneDrive/Bureau/python
    $ mv job01.py starter-python
    
    AA@DESKTOP-U15881 MINGW64 ~/OneDrive/Bureau/python
    $ git add job01.py
    fatal: not a git repository (or any of the parent directories): .git
    
    AA@DESKTOP-U15881 MINGW64 ~/OneDrive/Bureau/python
    $ cd starter-python
    
    AA@DESKTOP-U15881 MINGW64 ~/OneDrive/Bureau/python/starter-python (main)
    $ git add job01.py
    
    AA@DESKTOP-U15881 MINGW64 ~/OneDrive/Bureau/python/starter-python (main)
    $ git commit -m ^C

---
+++ job06.py
@@ -0,0 +1,16 @@
+
+# un prompteur devra s’afficher “>”, et le programme devra attendre un input.
+# Si l’input entré est “Bonjour”, le programme devra répondre “Bonjour à toi”
+# Si l’input entré est “Au revoir”, le programme devra quitter
+
+n = (input(">"))
+while n != "Au revoir":
+    if n == "Bonjour":
+        print("Bonjour à toi")
+    else:
+        print(n)
+    n = (input(">"))
+
+
+
+
