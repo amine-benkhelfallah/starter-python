74ca854a07b0ca0b382cee887aee9d926fd8efca
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-10-19 12:41:15 +0200

    Les boucles infinies

--- job06.py
+++ job06.py
@@ -1,7 +1,8 @@
 
-# un prompteur devra s’afficher “>”, et le programme devra attendre un input.
-# Si l’input entré est “Bonjour”, le programme devra répondre “Bonjour à toi”
-# Si l’input entré est “Au revoir”, le programme devra quitter
+# Un prompteur devra s’afficher “>”, et le programme devra attendre un input.
+# Si l’input entré est “Bonjour”, le programme devra répondre “Bonjour à toi”.
+# Si l’input entré est “Au revoir”, le programme devra quitter.
+
 
 n = (input(">"))
 while n != "Au revoir":
