98dce39bff2eb5d27d38aa22db1abc6729ede8dd
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-10-19 16:22:50 +0200

    Dessiner un rectangle avec une entré input hateur

---
+++ triangle.py
@@ -0,0 +1,33 @@
+
+# Écrire un programme qui affiche dans le terminal un triangle avec des :
+#
+# "_"
+# "\"
+# "/"
+
+hauteur = int(input("Entrez la hauteur de votre traiangle: "))
+if hauteur == 0:
+    print("Size is null.")
+    exit()
+
+y = hauteur - 1 
+z = 0
+s = 1
+space = " "
+
+while s <= hauteur:
+    if s == hauteur:
+        print(y*space, end="")
+        print("/",end="")
+        print(z*"_",end="")
+        print("\\")
+    else:
+        print(y*space, end="")
+        print("/",end="")
+        print(z*space,end="")
+        print("\\")
+    s+=1
+    y-=1
+    z+=2
+
+    
