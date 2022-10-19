bf247c3af86b6026e6026b6951c1858ee1183518
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-10-19 22:51:32 +0200

    Programme avec myFunction écrit Bonjour prenom nom” dans le terminal

---
+++ job15.py
@@ -0,0 +1,11 @@
+#Fonctions et paramètres
+
+# Créer un programme qui devra contenir une fonction qui prend en
+# paramètres une variable prenom et une variable nom.
+# La fonction écrira “Bonjour prenom nom” dans le terminal.
+
+# A l’aide du mot-clé (def) une fonction est définie 
+def myFunction(prenom, nom):
+    print("Bonjour", prenom, nom)
+
+myFunction("Amine", "BENKHELFALLAH")     #toujours appeler ma fonction pour executer 
