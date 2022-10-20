0510251a36399ea455f475bba4beca9685513632
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-10-20 02:03:23 +0200

    Programme avec fonction myAddition

---
+++ job20.py
@@ -0,0 +1,13 @@
+#Valeur de retour
+
+# Créer un programme qui devra contenir une seule fonction
+# nommée myAddition qui prend en paramètres deux nombres et qui retourne le résultat de leur addition.
+# Vous devrez appeler cette fonction et assigner à une variable le résultat qu’elle retourne.
+
+
+def myAddition (a,b):
+    sum = a + b
+    return sum
+
+variable = myAddition(24,82)
+print(variable)
